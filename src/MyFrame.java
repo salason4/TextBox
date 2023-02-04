3d7218d789e0094ad8fce9e481502842b4075956
Engr Horshy <salawudeensodiq8@gmail.com>
2023-02-04 23:24:40 +0100

    Initial commit

---
+++ src/MyFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    MyFrame (){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+
+
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
